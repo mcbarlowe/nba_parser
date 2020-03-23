f2a72c89b08489e1c99e56546eef731d1a515aba
mcbarlowe <mcbarlowe@gmail.com>
2020-03-23 00:21:04 -0400

    Functions to calculate blocks/points written and tests pass

--- nba_parser/nba_parser.py
+++ nba_parser/nba_parser.py
@@ -1,9 +1,150 @@
+from datetime import datetime
+import numpy as np
+import pandas as pd
+
+
 class PbP:
-    '''
+    """
     This class represents one game of of an NBA play by play dataframe. I am
     building methods on top of this class to streamline the calculation of
     stats from the play by player and then insertion into a database of the
     users choosing
-    '''
+    """
+
     def __init__(self, pbp_df):
         self.df = pbp_df
+        self.home_team = pbp_df["home_team_abbrev"].unique()[0]
+        self.away_team = pbp_df["away_team_abbrev"].unique()[0]
+        self.home_team_id = pbp_df["home_team_id"].unique()[0]
+        self.away_team_id = pbp_df["away_team_id"].unique()[0]
+        self.game_date = datetime.strptime(pbp_df["game_date"].unique()[0], "%Y-%m-%d")
+        self.season = pbp_df["season"].unique()[0]
+
+    def _point_calc_player(self):
+        """
+        method calculates simple shooting stats like field goals, three points,
+        and free throws made and attempted.
+        """
+        self.df["fgm"] = np.where(
+            (self.df["shot_made"] == 1) & (self.df["event_type_de"] == "shot"), 1, 0
+        )
+        self.df["fga"] = np.where(
+            self.df["event_type_de"].str.contains("shot|missed_shot", regex=True), 1, 0
+        )
+        self.df["tpm"] = np.where(
+            (self.df["shot_made"] == 1) & (self.df["is_three"] == 1), 1, 0
+        )
+        self.df["tpa"] = np.where(self.df["is_three"] == 1, 1, 0)
+        self.df["ftm"] = np.where(
+            (self.df["shot_made"] == 1)
+            & (self.df["event_type_de"].str.contains("free-throw")),
+            1,
+            0,
+        )
+        self.df["fta"] = np.where(
+            self.df["event_type_de"].str.contains("free-throw"), 1, 0
+        )
+
+        player_points_df = (
+            self.df.groupby(["player1_id", "game_date", "game_id", "player1_team_id"])[
+                ["fgm", "fga", "tpm", "tpa", "ftm", "fta"]
+            ]
+            .sum()
+            .reset_index()
+        )
+        player_points_df["player1_team_id"] = player_points_df[
+            "player1_team_id"
+        ].astype(int)
+        player_points_df["game_date"] = pd.to_datetime(player_points_df["game_date"])
+        player_points_df.rename(
+            columns={"player1_id": "player_id", "player1_team_id": "team_id"},
+            inplace=True,
+        )
+
+        return player_points_df
+
+    def __assist_calc_player(self):
+        pass
+
+    def __rebound_calc_player(self):
+        pass
+
+    def __turnover_calc_player(self):
+        pass
+
+    def __foul_calc_player(self):
+        pass
+
+    def __steal_calc_player(self):
+        pass
+
+    def _block_calc_player(self):
+        """
+        function to calculate player blocks and return a dataframe with players
+        and blocked shots stats along with key columns to join to other dataframes
+        """
+        blocks = self.df[self.df["event_type_de"] != "jump-ball"]
+        blocks = (
+            blocks.groupby(["player3_id", "game_id", "game_date", "player3_team_id"])[
+                ["is_block"]
+            ]
+            .sum()
+            .reset_index()
+        )
+        blocks["game_date"] = pd.to_datetime(blocks["game_date"])
+        blocks["player3_team_id"] = blocks["player3_team_id"].astype(int)
+        blocks.rename(
+            columns={
+                "player3_id": "player_id",
+                "player3_team_id": "team_id",
+                "is_block": "blk",
+            },
+            inplace=True,
+        )
+
+        return blocks
+
+    def __plus_minus_player(self):
+        pass
+
+    def __point_calc_team(self):
+        pass
+
+    def __assist_calc_team(self):
+        pass
+
+    def __rebound_calc_team(self):
+        pass
+
+    def __turnover_calc_team(self):
+        pass
+
+    def __foul_calc_team(self):
+        pass
+
+    def __steal_calc_team(self):
+        pass
+
+    def __block_calc_team(self):
+        pass
+
+    def __plus_minus_team(self):
+        pass
+
+    def playerbygamestats(self):
+        """
+        this function combines all playerbygamestats and returns a dataframe
+        containing them
+        """
+        points = self._point_calc_player()
+        blocks = self._block_calc_player()
+
+        playerbygamestats = points.merge(
+            blocks, how="left", on=["player_id", "team_id", "game_date", "game_id"]
+        )
+        playerbygamestats["blk"] = playerbygamestats["blk"].fillna(0).astype(int)
+
+        return playerbygamestats
+
+    def teambygamestats(self):
+        pass
--- test/test_unit/test_class_construction.py
+++ test/test_unit/test_class_construction.py
@@ -9,15 +9,74 @@ def setup():
     function for test setup and teardown
     """
     pbp_df = pd.read_csv("test/20700233.csv")
-    yield pbp_df
+    pbp_df["season"] = 2008
+    pbp = PbP(pbp_df)
+    # TODO add multiple files here to make the tests more random and more
+    # robust
+    yield pbp
 
 
 def test_class_build(setup):
     """
     This test makes sure the proper class is instantiated when called
     """
-    setup_df = setup
-    pbp = PbP(setup_df)
+
+    pbp = setup
 
     assert isinstance(pbp, PbP)
     assert isinstance(pbp.df, pd.DataFrame)
+    assert pbp.home_team == "DEN"
+    assert pbp.away_team == "LAC"
+    assert pbp.home_team_id == 1610612743
+    assert pbp.away_team_id == 1610612746
+    assert pbp.game_date.strftime("%Y-%m-%d") == "2007-11-30"
+    assert pbp.season == 2008
+
+
+def test_point_calc_player(setup):
+    """
+    testing to make sure points, field goals attempted/made, three pointers
+    attempted/made, and free throws attempted/made are correct
+    """
+
+    pbp = setup
+
+    stats_df = pbp._point_calc_player()
+
+    assert stats_df.loc[stats_df["player_id"] == 1894, "fgm"].values[0] == 10
+    assert stats_df.loc[stats_df["player_id"] == 1894, "fga"].values[0] == 20
+    assert stats_df.loc[stats_df["player_id"] == 1894, "tpm"].values[0] == 1
+    assert stats_df.loc[stats_df["player_id"] == 1894, "tpa"].values[0] == 2
+    assert stats_df.loc[stats_df["player_id"] == 1894, "ftm"].values[0] == 5
+    assert stats_df.loc[stats_df["player_id"] == 1894, "fta"].values[0] == 6
+    assert stats_df.loc[stats_df["player_id"] == 947, "fgm"].values[0] == 11
+    assert stats_df.loc[stats_df["player_id"] == 947, "fga"].values[0] == 26
+    assert stats_df.loc[stats_df["player_id"] == 947, "tpm"].values[0] == 0
+    assert stats_df.loc[stats_df["player_id"] == 947, "tpa"].values[0] == 2
+    assert stats_df.loc[stats_df["player_id"] == 947, "ftm"].values[0] == 4
+    assert stats_df.loc[stats_df["player_id"] == 947, "fta"].values[0] == 6
+
+
+def test_block_calc_player(setup):
+    """
+    testing to make sure block calculations are computing properly
+    """
+
+    pbp = setup
+
+    blocks = pbp._block_calc_player()
+    stats_df = pbp._point_calc_player()
+
+    # merging with normal player stats to make sure that players with
+    # zero block are properly calculated as well
+
+    stats_df = stats_df.merge(
+        blocks, how="left", on=["player_id", "team_id", "game_date", "game_id"]
+    )
+    stats_df["blk"] = stats_df["blk"].fillna(0).astype(int)
+
+    assert stats_df.loc[stats_df["player_id"] == 1894, "blk"].values[0] == 0
+    assert stats_df.loc[stats_df["player_id"] == 947, "blk"].values[0] == 0
+    assert stats_df.loc[stats_df["player_id"] == 948, "blk"].values[0] == 1
+    assert stats_df.loc[stats_df["player_id"] == 2549, "blk"].values[0] == 3
+    assert stats_df.loc[stats_df["player_id"] == 2059, "blk"].values[0] == 1
